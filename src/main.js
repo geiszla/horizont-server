3f82156fc50a2cadfe0e9c0141dca5a921e9ab25
Andras Geiszl <geiszla@gmail.com>
2019-06-28 16:49:10 +0100

    Secret decryption

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-require('@google-cloud/debug-agent').start();
-
+const bluebird = require('bluebird');
 /** @type {object} chalk */
 const chalk = require('chalk');
-
-const { databaseAddress } = require('../appconfig.json');
+const debugAgent = require('@google-cloud/debug-agent');
+const kms = require('@google-cloud/kms');
+const readFileAsync = bluebird.promisify(require('fs').readFile);
 
 const { print } = require('./log');
 const { createWebserverAsync } = require('./webserver');
@@ -24,12 +24,29 @@ const IS_CLUSTERING = false;
     startWithClusteringAsync(IS_PRODUCTION, PORT);
   } else {
     let logLevel = 0;
-    if (!IS_PRODUCTION) {
+
+    let databaseAddress = 'mongodb://localhost:27017';
+    if (IS_PRODUCTION) {
+      // debugAgent.start();
+
+      const kmsClient = new kms.KeyManagementServiceClient({ projectId: 'horizont-245015' });
+      const keyPath = kmsClient.cryptoKeyPath('horizont-245015', 'global', 'horizont', 'secrets');
+
+      const encryptedContent = await readFileAsync('./appconfig.json.enc');
+      const kmsDecryptRequest = {
+        name: keyPath,
+        ciphertext: encryptedContent.toString('base64'),
+      };
+
+      const [decryptedContent] = await kmsClient.decrypt(kmsDecryptRequest);
+      const configData = JSON.parse(decryptedContent.plaintext.toString('utf-8'));
+      ({ databaseAddress } = configData.databaseAddress);
+    } else {
       logLevel = process.argv.includes('--verbose') ? 2 : 1;
     }
 
     // Start Webserver
-    startWebserverAsync(2);
+    startWebserverAsync(2, databaseAddress);
   }
 })();
 
@@ -37,8 +54,9 @@ const IS_CLUSTERING = false;
 
 /**
  * @param {number} logLevel
+ * @param {string} databaseAddress
  */
-async function startWebserverAsync(logLevel) {
+async function startWebserverAsync(logLevel, databaseAddress) {
   const app = await createWebserverAsync(databaseAddress, logLevel);
 
   app.listen(PORT, () => {
